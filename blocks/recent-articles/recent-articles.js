9ebb9bfa9f688381d88c319e096001867553b767
Tucker Elliott <telliott@adobe.com>
2023-03-29 17:10:48 -0400

    fixes to recent articles

--- blocks/recent-articles/recent-articles.js
+++ blocks/recent-articles/recent-articles.js
@@ -2,19 +2,23 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js'
 
 export default async function decorate(block) {
     const { href } = block.firstElementChild.querySelector('a')
-    const resp = await fetch(href)
+    const resp = await fetch(href) 
     if (resp.ok) {
         const { data } = await resp.json()
-        const teasers = data.map(({ path, image, description, title }) => {
+        const teasers = data.map(({ path, image, lastModified, title, author}) => {
             const picture = createOptimizedPicture(image, '', false, [{ width: 400 }])
             return `
                 <a href="${path}" title="${title}" class="recent-article">
                     ${picture.outerHTML}
-                    <h4>${title}</h4>
-                    <p>${description}</p>
+                    <h3>Storage</h3>
+                    <h1>${title}</h1>
+                    <h5>BY ${author}</h5>
+                    <h6>MARCH 28, 2023, 11:34 AM EDT</h6>
+                    <!-- <p>${lastModified}</p> -->
                 </a>`
         });
-        block.innerHTML = `<ul>${teasers.map(teaser => `<li>${teaser}</li>`).join('\n')}</ul>`
+       block.innerHTML = `<ul>${teasers.map(teaser => `<br><li>${teaser}</li>`).join('\n')}</ul>`
+     //   block.innerHTML = `<div>${teasers.map(teaser => `<div>${teaser}<//div>`).join('\n')}</div>`
     } else {
         block.remove()
     }
