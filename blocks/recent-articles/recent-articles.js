5c2298f68305334001e3b2269818ef2128bc5121
Tucker Elliott <telliott@adobe.com>
2023-03-30 10:27:08 -0400

    added category

--- blocks/recent-articles/recent-articles.js
+++ blocks/recent-articles/recent-articles.js
@@ -5,12 +5,12 @@ export default async function decorate(block) {
     const resp = await fetch(href) 
     if (resp.ok) {
         const { data } = await resp.json()
-        const teasers = data.map(({ path, image, lastModified, title, author}) => {
+        const teasers = data.map(({ path, image, lastModified, title, author, theme}) => {
             const picture = createOptimizedPicture(image, '', false, [{ width: 400 }])
             return `
                 <a href="${path}" title="${title}" class="recent-article">
                     ${picture.outerHTML}
-                    <h3>Storage</h3>
+                    <h3>${theme}</h3>
                     <h1>${title}</h1>
                     <h5>BY ${author}</h5>
                     <h6>MARCH 28, 2023, 11:34 AM EDT</h6>
